7b97fb770547388944c32534acad07cc24dd1f5b
syh8088 <syh8088@gmail.com>
2024-06-30 10:04:24 +0900

    PaymentService 개발 (2)
    PaymentService 개발 (2)

--- 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/PaymentService.java
+++ 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,14 +1,21 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
 
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+
+        // 환율 가져오기
+        // 금액 계산
+        // 유효시간 계산
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment prepare = paymentService.prepare();
+        Payment prepare = paymentService.prepare(100L, "UDD", BigDecimal.valueOf(50.7));
         System.out.println("prepare = " + prepare);
     }
 }
