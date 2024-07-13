f5af7fa8124998e673198fcd9af5a27c8554ed40
syh8088 <syh8088@gmail.com>
2024-07-13 22:38:15 +0900

    ApiExecutor 콜백과 메소드 주입
    ApiExecutor 콜백과 메소드 주입

--- 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -2,6 +2,7 @@ package tobyspring.hellospring.exrate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import tobyspring.hellospring.api.ApiExecutor;
 import tobyspring.hellospring.api.SimpleApiExecute;
 import tobyspring.hellospring.payment.ExRateProvider;
 
@@ -22,10 +23,10 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url);
+        return runApiForExRate(url, new SimpleApiExecute());
     }
 
-    private static BigDecimal runApiForExRate(String url) {
+    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor) {
         URI uri;
         try {
             uri = new URI(url);
@@ -35,7 +36,7 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         String response;
         try {
-            response = new SimpleApiExecute().execute(uri);
+            response = apiExecutor.execute(uri);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
