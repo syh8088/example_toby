6ef077e0b63a5dc594b479d1272169fd6c3405ed
syh8088 <syh8088@gmail.com>
2024-07-13 21:28:00 +0900

    변하는 코드 분리하기 - 메소드 추출
    변하는 코드 분리하기 - 메소드 추출

--- 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -32,31 +32,39 @@ public class WebApiExRateProvider implements ExRateProvider {
         try {
             HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 
-            try(
-                    BufferedReader br = new BufferedReader(// 사람이 알아보기 쉽게 텍스트 한줄 한줄 가져옴
-                        new InputStreamReader( // 우리가 알아볼수 있는 형태로 변환 (캐릭터 형태로 변경)
-                                connection.getInputStream() // 파일이나 네트워트 통해서 넘겨오는 데이터를 바이트 형태로 변환
-                        )
-                    )
-            ) {
-                response = br.lines().collect(Collectors.joining("\n"));
-            }
+            response = executeApi(connection);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        ObjectMapper mapper = new ObjectMapper();
-
         // mapper 의 정보를 가져온다. java Object 로 변환
         try {
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-
-            System.out.println("API ExRate: " + data.rates().get("KRW"));
-
-            return data.rates().get("KRW");
+            return parseExRate(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static BigDecimal parseExRate(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        System.out.println("API ExRate: " + data.rates().get("KRW"));
+
+        return data.rates().get("KRW");
+    }
+
+    private static String executeApi(HttpURLConnection connection) throws IOException {
+        String response;
+        try(
+                BufferedReader br = new BufferedReader(// 사람이 알아보기 쉽게 텍스트 한줄 한줄 가져옴
+                    new InputStreamReader( // 우리가 알아볼수 있는 형태로 변환 (캐릭터 형태로 변경)
+                            connection.getInputStream() // 파일이나 네트워트 통해서 넘겨오는 데이터를 바이트 형태로 변환
+                    )
+                )
+        ) {
+            response = br.lines().collect(Collectors.joining("\n"));
+        }
+        return response;
     }
 
 }
