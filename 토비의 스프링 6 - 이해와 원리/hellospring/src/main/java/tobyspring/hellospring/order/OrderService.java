43b8422d61b236309c2888add55e41d3c0b81c37
syh8088 <syh8088@gmail.com>
2024-07-20 20:16:09 +0900

    트랜잭션 테스트
    트랜잭션 테스트

--- 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/order/OrderService.java
+++ 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -24,6 +25,15 @@ public class OrderService {
         return new TransactionTemplate(transactionManager).execute(status -> {
             this.orderRepository.save(order);
             return order;
+        });
+    }
+
+    public List<Order> createOrders(List<OrderReq> reqs) {
+
+        return new TransactionTemplate(transactionManager).execute(status -> {
+            return reqs.stream()
+                    .map(req -> createOrder(req.no(), req.total()))
+                    .toList();
         });
     }
 }
--- 토비의 스프링 6 - 이해와 원리/hellospring/src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
+++ 토비의 스프링 6 - 이해와 원리/hellospring/src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -3,13 +3,18 @@ package tobyspring.hellospring.order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.hellospring.OrderConfig;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * JUnit 이 이 ContextConfiguration 이용 할려면 이게
@@ -22,6 +27,9 @@ class OrderServiceSpringTest {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
     void createOrder() {
 
@@ -29,4 +37,35 @@ class OrderServiceSpringTest {
         assertThat(order.getId()).isGreaterThan(0);
     }
 
+    @Test
+    void createOrders() {
+
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0200", BigDecimal.ONE),
+                new OrderReq("0201", BigDecimal.TWO)
+        );
+
+        var orders = orderService.createOrders(orderReqs);
+        assertThat(orders).hasSize(2);
+        orders.forEach(order -> {
+            assertThat(order.getId()).isGreaterThan(0);
+        });
+    }
+
+    @Test
+    void createDuplicatedOrders() {
+
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.ONE),
+                new OrderReq("0300", BigDecimal.TWO)
+        );
+
+        assertThatThrownBy(() -> {
+            orderService.createOrders(orderReqs);
+        }).isInstanceOf(DataIntegrityViolationException.class);
+
+        JdbcClient client = JdbcClient.create(this.dataSource);
+        var count = client.sql("select count(*) from orders where no = '0300'").query(Long.class).single();
+        assertThat(count).isEqualTo(0);
+    }
 }
