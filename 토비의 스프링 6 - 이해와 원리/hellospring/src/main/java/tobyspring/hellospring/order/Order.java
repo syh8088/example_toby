6ae0570e8a4c793ff00a3c976d73e5da1ea47f3e
syh8088 <syh8088@gmail.com>
2024-07-20 10:44:41 +0900

    기술에 독립적인 애플리케이션 서비스
    기술에 독립적인 애플리케이션 서비스

--- 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/order/Order.java
+++ 토비의 스프링 6 - 이해와 원리/hellospring/src/main/java/tobyspring/hellospring/order/Order.java
@@ -4,14 +4,14 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
+//@Entity
+//@Table(name = "orders")
 public class Order {
 
-    @Id @GeneratedValue
+//    @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
+//    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
@@ -21,6 +21,30 @@ public class Order {
 
     public Order(String no, BigDecimal total) {
         this.no = no;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
         this.total = total;
     }
 
